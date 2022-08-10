d9418cb6baf3c374e6e1bdeb6cfcf323507fd3d9
LMata-Facto <lmata@facto.cat>
2022-08-10 14:45:21 +0200

    new change env and url to call api

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -13,8 +13,8 @@ export function createApollo(httpLink: HttpLink) {
   console.log('**** Env Uri: ', `http://${environment.apiUrl}`);
     return {
       link: httpLink.create({
-        //uri: `http://${environment.apiUrl}/graphql`,
-        uri: ServerUri
+        uri: `http://${environment.apiUrl}/graphql`,
+        //uri: ServerUri
       }),
       cache: new InMemoryCache(),
     };
