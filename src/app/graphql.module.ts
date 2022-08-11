b29c26f62626de00dced2c46cfaafebdbd44c878
LMata-Facto <lmata@facto.cat>
2022-08-11 11:16:41 +0200

    modify conf. to get data from api

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -4,17 +4,15 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client/core';
-import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment';
 
-//const localUri = 'http://localhost:3000/graphql';
-const ServerUri = 'http://api-factodev.upmeup.es';
+
+//uri: `http://${environment.apiUrl}/graphql`,
 
 export function createApollo(httpLink: HttpLink) {
-  //console.log('**** Env Uri: ', `http://${environment}`);
     return {
       link: httpLink.create({
-        //uri: `http://${environment.apiUrl}/graphql`,
-        uri: ServerUri
+        uri: '/graphql'
       }),
       cache: new InMemoryCache(),
     };
